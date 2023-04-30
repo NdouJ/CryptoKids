73f7fa3041cdf7434e65865ab45227a6233e0c6e
NdouJ <jelena.ndou@gmail.com>
2023-04-30 08:51:55 +0200

    Initial commit

---
+++ KidscryptoApi/Controllers/CryptoFactsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using KidscryptoApi.Models;
+using KidscryptoAp.Data;
+
+namespace KidscryptoApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CryptoFactsController : ControllerBase
+    {
+
+        private readonly ApiContext _context;
+        public CryptoFactsController(ApiContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpGet]
+        public JsonResult GetCryptoFact(){
+
+     
+            int count = _context.factories.Count();
+
+            int randomIndex = new Random().Next(0, count);
+
+   
+            object? randomCryptoFact = _context.factories.Skip(randomIndex).FirstOrDefault();
+
+            object? randumCryptoFact = null;
+            return new JsonResult(Ok(randumCryptoFact));
+
+        }
+
+        [HttpGet()]
+        public JsonResult GetAll(){
+        
+        var result = _context.factories.ToList();
+
+        return new JsonResult(Ok(result));      
+        }
+
+    }
+}
---
+++ KidscryptoApi/Models/CryptoFact.cs
@@ -0,0 +1,11 @@
+﻿namespace KidscryptoApi.Models
+{
+ 
+        public class CryptoFact
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Fact { get; set; }
+        }
+    
+}
